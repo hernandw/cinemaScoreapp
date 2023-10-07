8788167f0790a070984b0e16043d46385c3ce0a8
Williams Hernández <hernandw@gmail.com>
2023-10-07 09:48:37 -0300

    :sparkles: estilos de las card

--- src/components/CardMovies.jsx
+++ src/components/CardMovies.jsx
@@ -1,19 +1,29 @@
 import { useContext } from "react";
 import { ContextMovie } from "../context/ContextMovie";
 
-const CardMovies = ({ backdrop_path, title, overview }) => {
-  const { imageBase } = useContext(ContextMovie);
+const CardMovies = ({ poster_path, title, overview, vote_average }) => {
+    const { imageBase } = useContext(ContextMovie);
+    const getColor = (color)=> {
+      if(color >= 8){
+        return "success";
+      }else if(color >= 5){
+        return "warning";
+      }else{
+        return "danger";
+      }
+    }
   return (
     
       <div className="w-25 cardmovie">
         <img
-          src={imageBase + backdrop_path}
+          src={imageBase + poster_path}
           className="card-img-top"
           alt={title}
         />
-        <div className="card-body p-2">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{overview}</p>
+        <div className="card-body p-4 d-flex justify-content-between">
+              <h5 className="card-title">{title}</h5>
+              <p className={`text-${getColor(vote_average)} fw-bold bg-color average`}>{vote_average}</p>
+          {/* <p className="card-text">{overview}</p> */}
         </div>
       </div>
     
